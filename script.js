d53757449bdda0c7fd3d6c3d2613dbae9f67b07f
leticea <nascimento.leticeah@gmail.com>
2023-03-10 13:34:43 -0300

    separando as consts

---
+++ script.js
@@ -0,0 +1,10 @@
+const switchFrm = document.querySelector("#switch-frm");
+const switchF1 = document.querySelector("#switch-f1");
+const switchF2 = document.querySelector("#switch-f2");
+const switchCircle = document.querySelectorAll(".switch-circle");
+const switchBtn = document.querySelectorAll(".switch-btn");
+const regFrm = document.querySelector("#register-form");
+const logFrm = document.querySelector("#login-form");
+const allButtons = document.querySelectorAll(".submit");
+
+
